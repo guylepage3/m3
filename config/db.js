b26124a3888882f01cd97ac7e6de15c5c5ece993
Guy Lepage <glepage@onename.com>
2015-11-23 09:58:21 -0500

    updated db url in config

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
     module.exports = {
-        url : 'mongodb://localhost/stencil-dev'
+        url : 'mongodb://<harmony>:<*HP7BqB>mm-8>7Up>@ds057244.mongolab.com:57244/harmonydb'
     }
