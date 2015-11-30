30eabef0b57d00d5450b6e3bff32bf5fb8a8d346
Guy Lepage <glepage@onename.com>
2015-11-30 08:24:06 -0500

    created angular approutes

---
+++ public/js/appRoutes.js
@@ -0,0 +1,20 @@
+// public/js/appRoutes.js
+    angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        // home page
+        .when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'MainController'
+        })
+
+        // nerds page that will use the NerdController
+        .when('/nerds', {
+            templateUrl: 'views/nerd.html',
+            controller: 'NerdController'
+        });
+
+    $locationProvider.html5Mode(true);
+
+}]);
