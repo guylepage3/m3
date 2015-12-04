7fda43c8fe9a6c631d938d32dd01476957db5c08
Guy Lepage <glepage@onename.com>
2015-12-04 14:16:06 -0500

    removed old jumbotron from home view and updated home tagline

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 // public/js/controllers/MainCtrl.js
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-    $scope.tagline = 'To the moon and back!';   
+    $scope.tagline = 'An ultra-light UI framework built for designers';   
 
 });
