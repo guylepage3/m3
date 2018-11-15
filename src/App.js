9cd3809596ad9791ff837be17b8748dc6b12e8e7
Guy Lepage <guylepage3@gmail.com>
2018-11-15 12:33:35 -0500

    added line endings to fix circleci build issue #12
    Signed-off-by: Guy Lepage <guylepage3@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import { Route, Switch } from "react-router-dom";
 import './styles/App.css';
 import Home from './components/Home';
 import NoMatch from './components/NoMatch';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faAngleUp, faAngleRight, faExternalLinkAlt, faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faAngleUp, faAngleRight, faExternalLinkAlt, faLongArrowAltUp } from '@fortawesome/free-solid-svg-icons';
 
 library.add(fab, faAngleUp, faAngleRight, faExternalLinkAlt, faLongArrowAltUp)
 
